cc9130e0c2f6ee1a2cbbd9a87364785ff8ec4029
farrelhafidzulahsan <111212981+farrelhafidzulahsan@users.noreply.github.com>
2022-10-09 14:27:12 +0700

    Operator,Logical,Comparison
    operator,logical,comparison

---
+++ Operator.py
@@ -0,0 +1,14 @@
+
+# Addition
+a = int(input("Masukan Bilangan Satu :"))
+b = int(input("Masukan Bilangan Dua :"))
+
+# Arithmetic Operators
+
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * b = {a * b}")
+print(f"a / b = {a / b}")
+print(f"a % b = {a % b}")
+print(f"a ** b = {a ** b}")
+print(f"a // b = {a // b}")
