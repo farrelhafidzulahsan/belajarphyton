ddacec26e6551cb44f6b4c04f1ebae129f6405c8
farrelhafidzulahsan <111212981+farrelhafidzulahsan@users.noreply.github.com>
2022-10-30 15:01:45 +0700

    Coding
    materiu coding hari ini 30/10/22

---
+++ ForLoop.py
@@ -0,0 +1,9 @@
+ number = int(input('insert Number :'))
+
+ for iterasi in range(number):
+      # print(f'Nilai ke {iterasi}')
+      print(iterasi,end='')
+      
+
+# membuat urutan 1-10
+# 1
--- TugasIfElse.py
+++ TugasIfElse.py
@@ -12,5 +12,30 @@
  # fisika
 
 # 1. buat variable mata pelajaran
+matematika = int(input('Matematika :'))
+english = int(input('English :'))
+b_indonesia = int(input('Indonesia :'))
+
 # 2. biar nilai rata rata (mat + indo)/5
+nilai_rata_rata= (matematika + english+ b_indonesia) / 3
+print(f'nilai rata rata {nilai_rata_rata} ')
+
+
+print(f'Nilai rata rata {nilai_rata_rata} ')
+if (nilai_rata_rata >= 90 and nilai_rata_rata <= 100):
+    print(f'Nilai {nilai_rata_rata} mendapatkan A')
+elif (nilai_rata_rata >= 80 and nilai_rata_rata < 90):
+    print(f'Nilai {nilai_rata_rata} mendapatkan B')
+elif (nilai_rata_rata >= 70 and nilai_rata_rata < 80):
+    print(f'Nilai {nilai_rata_rata} mendapatkan C')
+elif (nilai_rata_rata >= 60 and nilai_rata_rata < 70):
+    print(f'Nilai {nilai_rata_rata} mendapatkan D')
+elif (nilai_rata_rata < 60):
+    print(f'Nilai {nilai_rata_rata} mendapatkan E')
+else:
+    print(f'Nilai {nilai_rata_rata} Tidak dalam Rentang')
+
+# total = 5 + 6 * 2
+# print(f'Total {total} ')
+
 # 3. nilai rata rata di selection
